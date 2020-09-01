1e5715d7b0218863c8479063278d138241065797
Misbah-D <misbah.deeb@gmail.com>
2020-09-01 13:44:52 +0200

    ok

--- football.js
+++ football.js
@@ -1,1 +1,18 @@
+et team1;
+let team2;
+function gameResults(team1, team2) {  
+    if (team1 > team2) {
+        console.log(`Team1 gets ${team1 = 3} points and Team2 gets ${team2 = 0} points`);
+    }
+
+     else if (team1 < team2) { console.log(`Team1 gets ${ team1=0 } points and Team2 gets ${ team2=3 } Points`);
+            
+    }
+    else {
+            console.log(`both Teams gets ${ team1=1 } and ${ team2=1 } points`);
+            
+        }
+        
+    }
+gameResults(12, 7) // Team1 gets 3 points and Team2 gets 0 points//
 
